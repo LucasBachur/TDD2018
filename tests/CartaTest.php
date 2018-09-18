8de5d97c57ab8c624f84f9c69320760d9efbc93d
LucasBachur <37964980+LucasBachur@users.noreply.github.com>
2018-09-18 17:08:31 -0300

    Agregado test ver palo

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -5,9 +5,22 @@ namespace TDD;
 use PHPUnit\Framework\TestCase;
 
 class CartaTest extends TestCase {
+  
   public function testExiste(){
-    $carta = new Carta;
+    $palo = "Espada";
+    $numero = "1";
+    
+    $carta = new Carta($palo, $numero);
+    
     $this->assertTrue(isset($carta));
+  }
+  
+  public function testVerPalo(){
+    $palo = "Basto";
+    $numero = "7";
+    
+    $carta = new Carta($palo, $numero);
+    $this->assertEquals($this->obtenerPalo(),$palo);
   }
 }
 
